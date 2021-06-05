2778a24c89853d8286a3727ef8727beff2a8084a
TiPu SuLtAn <bdsoldire@gmail.com>
2021-06-05 16:08:28 +0600

    react-footer

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FooterContainer } from "./containers/footer";
+
+function App() {
+  return (
+    <div className="App">
+      <FooterContainer />
+    </div>
+  );
+}
+
+export default App;
